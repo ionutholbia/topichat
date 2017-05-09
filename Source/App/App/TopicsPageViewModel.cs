561c37a2a28e49bd755b4596dd12ba04022f69f0
Ciprian-Ionut Holbia <ciprian-ionut.holbia@avira.com>
2017-05-09 21:34:00 +0200

    Added header to topics page.

--- Source/App/App/TopicsPageViewModel.cs
+++ Source/App/App/TopicsPageViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Topichat.Core;
+using Xamarin.Forms;
 
 namespace Topichat.Forms
 {
@@ -10,11 +13,18 @@ namespace Topichat.Forms
         public TopicsPageViewModel()
         {
             Conversations = App.ConversationManager.GetConversations();
+            SearchCommand = new Command<string>(async(text) => await SearchInContactList(text));
             Participants = Conversations.FirstOrDefault().Participants.FirstOrDefault().FirstName;
         }
 
         public ObservableCollection<Conversation> Conversations { get; }
 
         public string Participants { get; }
+
+        public ICommand SearchCommand { get; private set; }
+
+        async Task SearchInContactList(string text)
+        {
+        }
     }
 }
