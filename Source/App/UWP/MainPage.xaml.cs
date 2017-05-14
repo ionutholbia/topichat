79a8f8e4a1548a8249e13803ad8fcf482d372a6b
Ciprian-Ionut Holbia <ionut.holbia@hotmail.com>
2017-05-14 12:35:11 +0200

    Added UWP project and WIN sln.

---
+++ Source/App/UWP/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Topichat.Forms;
+using Topichat.Core;
+using Topichat.Shared;
+
+namespace Topichat.App.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new Topichat.Forms.App(new ContactManager(),
+                new ConversationManager(
+                    new BrokerConnection("00400744360800"),
+                    new StorageData(),
+                    new Contact { FirstName = "Ionut", LastName = "Holbia", PhoneNumber = "00400744360800" })));
+
+        }
+    }
+}
